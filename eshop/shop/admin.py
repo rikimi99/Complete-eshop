166b49117e1614edacac1d9c2ccb7a5a1a1cfb91
rikimi99 <matiriki94@gmail.com>
2024-02-10 08:32:31 +0200

    #

--- eshop/shop/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Category, Product, Order
-
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(Profile)
