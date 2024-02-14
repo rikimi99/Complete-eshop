5ec8c9f9fe31df56d06f652f82bc99ed1b7fff90
RikiMati <R.Mati@acg.edu>
2024-02-14 18:53:03 +0200

    #

--- eshop/eshop/settings.py
+++ eshop/eshop/settings.py
@@ -121,8 +121,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rikimati@rikimati.com'
-EMAIL_HOST_PASSWORD = 'RatM233099.-/'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # Session cookie age (in seconds):
 SESSION_COOKIE_AGE = 60 * 60 * 24 * 30  # 30 days.
