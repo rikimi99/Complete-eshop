57e689aee107f2bfbcc832624b329e9f5b4892e6
rikimi99 <146011509+rikimi99@users.noreply.github.com>
2024-02-14 16:08:59 +0200

    Update settings.py

--- eshop/eshop/settings.py
+++ eshop/eshop/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x5g(2ccl0_sq!w#n)%jw*53j3+9yd^jkb$_0^fckovofyzbcds'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
